fe333e58603a07c9b91a244f45ff3b756093fa76
Shantaroy07 <shantaroy148@gmail.com>
2022-10-27 01:23:19 +0600

    remove file

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express')
+var cors = require('cors')
 const app = express();
+app.use(cors())
 const port = process.env.PORT || 5000;
 
 
-app.get('/', (req, res) => {
-    res.send('News API Running')
-});
+const categories = require('./data/categories.json');
+
+app.get('/categories', (req, res) => {
+    res.send(categories)
+})
 
 app.listen(port, () => { console.log('running') })
